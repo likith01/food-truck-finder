35171bef6f014a245ddae9495a617e3be857841a
Likith S <likiths2501@gmail.com>
2026-08-23 17:28:17 +0530

    Added Cors Security To API Layer

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routes.food_trucks import router as food_trucks_router
 from app.api.routes.health import router as health_router
@@ -16,10 +17,20 @@ app = FastAPI(
 )
 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:4200",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(
     health_router,
 )
-
 
 app.include_router(
     food_trucks_router,
